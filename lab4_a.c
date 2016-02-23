cfae0b45007c2cd5559c79f81f8bf0dd2a225234
Mikhail Pospelov <mixan946@gmail.com>
2016-02-23 17:40:39 +0300

    init

--- lab4_a.c
+++ lab4_a.c
@@ -9,7 +9,6 @@
 #include <sys/time.h>
 
 #define _REENTRANT
-#define _PRINT_TO_FILE
 
 int main(int argc, char **argv) {
 
@@ -39,11 +38,11 @@ int main(int argc, char **argv) {
 
   FILE *fp;
 
-  nodes = 4096000;
+  nodes = 512;
   sub_nodes = 0;
   modelingTime = 0.012;
   //a_t = (210.0/(880.0*2700.0));
-  a_t=2;
+  a_t = 30;
   deltaT = 0.0001;
   deltaX = 0.1;
 
@@ -57,8 +56,8 @@ int main(int argc, char **argv) {
 
   if (!myrank) {
     for(i=0; i<nodes; i++) {
-        t_n[i]  = 0;
-        t_n1[i] = 0;
+      t_n[i]  = 0;
+      t_n1[i] = 0;
     }
   };
 
@@ -79,15 +78,15 @@ int main(int argc, char **argv) {
     T_n1[i] = 0;
   }
 
-#ifdef PRINT_TO_FILE
-  if(!myrank) {
-    fp = fopen("./animation.plot","w");
-    fprintf(fp, "set xrange[0:%d]\n", nodes);
-    fprintf(fp, "set yrange[-3:10]\n");
-  }
-#endif
+  #ifdef PRINT_TO_FILE
+    if(!myrank) {
+      fp = fopen("./animation.plot","w");
+      fprintf(fp, "set xrange[0:%d]\n", nodes);
+      fprintf(fp, "set yrange[-3:10]\n");
+    }
+  #endif
 
-//  printf("RANK: %d, sub_nodes: %d\n", myrank, sub_nodes);
+  // printf("RANK: %d, sub_nodes: %d\n", myrank, sub_nodes);
 
   if(!myrank) {
     gettimeofday(&start,NULL);
@@ -99,9 +98,9 @@ int main(int argc, char **argv) {
   down = myrank-1;
 
   if(myrank == 0)
-	down = MPI_PROC_NULL;
+  down = MPI_PROC_NULL;
   if(myrank == (total-1))
-	up = MPI_PROC_NULL;
+  up = MPI_PROC_NULL;
 
   while(currentTime < modelingTime) {
     //printf("Time: %f\n", currentTime);
@@ -112,24 +111,24 @@ int main(int argc, char **argv) {
     left_b = T_n1[0];
 
     if ((myrank % 2) == 0) {
-	//printf("RANK:%d HERE 1 sendTo:%d \n", myrank, up);
-	MPI_Send(&T_n1[sub_nodes-1], 1, MPI_DOUBLE, up, 0, MPI_COMM_WORLD);
- 	MPI_Recv(&right_b, 1, MPI_DOUBLE,  up, 1, MPI_COMM_WORLD, &status);
+      //printf("RANK:%d HERE 1 sendTo:%d \n", myrank, up);
+      MPI_Send(&T_n1[sub_nodes-1], 1, MPI_DOUBLE, up, 0, MPI_COMM_WORLD);
+      MPI_Recv(&right_b, 1, MPI_DOUBLE,  up, 1, MPI_COMM_WORLD, &status);
     }
     else {
-	//printf("RANK:%d HERE 1 sendTo:%d \n", myrank, down);
-	MPI_Recv(&left_b,  1, MPI_DOUBLE, down, 0, MPI_COMM_WORLD, &status);
-        MPI_Send(&T_n1[0], 1, MPI_DOUBLE, down, 1, MPI_COMM_WORLD);
+     //printf("RANK:%d HERE 1 sendTo:%d \n", myrank, down);
+      MPI_Recv(&left_b,  1, MPI_DOUBLE, down, 0, MPI_COMM_WORLD, &status);
+      MPI_Send(&T_n1[0], 1, MPI_DOUBLE, down, 1, MPI_COMM_WORLD);
     }
     if ((myrank % 2) == 1) {
-	//printf("RANK:%d HERE 2 sendTo:%d \n", myrank, up);
-	MPI_Send(&T_n1[sub_nodes-1], 1, MPI_DOUBLE, up, 2, MPI_COMM_WORLD);
-        MPI_Recv(&right_b,  1, MPI_DOUBLE, up, 3, MPI_COMM_WORLD, &status);
+      //printf("RANK:%d HERE 2 sendTo:%d \n", myrank, up);
+      MPI_Send(&T_n1[sub_nodes-1], 1, MPI_DOUBLE, up, 2, MPI_COMM_WORLD);
+      MPI_Recv(&right_b,  1, MPI_DOUBLE, up, 3, MPI_COMM_WORLD, &status);
     }
     else {
-	//printf("RANK:%d HERE 2 sendTo:%d \n", myrank, down);
-	MPI_Recv(&left_b, 1, MPI_DOUBLE, down, 2, MPI_COMM_WORLD, &status);
-        MPI_Send(&T_n1[0], 1,MPI_DOUBLE, down, 3, MPI_COMM_WORLD);
+      //printf("RANK:%d HERE 2 sendTo:%d \n", myrank, down);
+      MPI_Recv(&left_b, 1, MPI_DOUBLE, down, 2, MPI_COMM_WORLD, &status);
+      MPI_Send(&T_n1[0], 1,MPI_DOUBLE, down, 3, MPI_COMM_WORLD);
     }
 
     //printf("RANK: %d, left: %f, right: %f\n", myrank, left_b, right_b);
@@ -137,7 +136,7 @@ int main(int argc, char **argv) {
     for(i = 0; i<sub_nodes; i++) {
       if( (i + myrank*sub_nodes) == 0 ) { // GU 2
         //T_n1[i] = (1.)*deltaX/a_t + T_n1[i+1];
-        T_n1[i] = 3;
+        T_n1[i] = 5;
         continue;
       }
 
@@ -146,68 +145,55 @@ int main(int argc, char **argv) {
         continue;
       }
 
-     if( (i == 0) ) {
-         T_n1[i] = a_t*(left_b -
-           2*T_n1[i] + T_n1[i + 1])*deltaT/(deltaX*deltaX) + T_n1[i];
+      if(i == 0) {
+        T_n1[i] = a_t*(left_b - 2*T_n1[i] + T_n1[i + 1])*deltaT/(deltaX*deltaX) + T_n1[i];
         continue;
-     }
-
-     if( (i == sub_nodes-1) ) {
-         T_n1[i] = a_t*(T_n1[i - 1] -
-            2*T_n1[i] + right_b)*deltaT/(deltaX*deltaX) + T_n1[i];
-        continue;
-     }
-
-     if((i > 0) && (i < sub_nodes-1)) {
-
-     	T_n1[i] = a_t*(T_n1[i - 1] -
-       	2*T_n1[i] + T_n1[i + 1])*deltaT/(deltaX*deltaX) + T_n1[i];
-
-     	//printf("RANK %d is working %d from %d is %f\n", myrank, i, sub_nodes, T_n1[i]);
-     }
-
-#ifdef PRINT_TO_FILE
-    MPI_Gather((void *)T_n1, sub_nodes, MPI_DOUBLE,
-	       (void *)t_n1, sub_nodes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    if(!myrank) {
-      t_n = t_n1;
-      fprintf(fp, "plot '-' u 1:2 w linespoints lw 1\n");
-      for(j=0; j<nodes; j++) {
-         fprintf(fp, "%d %f\n",j, t_n[j]);
       }
-      fprintf(fp, "end\n");
-      fprintf(fp, "pause 0.0001\n");
-    }
-#endif
+
+      if(i == sub_nodes - 1) {
+        T_n1[i] = a_t*(T_n1[i - 1] - 2*T_n1[i] + right_b)*deltaT/(deltaX*deltaX) + T_n1[i];
+        continue;
+      }
+
+      if((i > 0) && (i < sub_nodes-1)) {
+        T_n1[i] = a_t*(T_n1[i - 1] - 2*T_n1[i] + T_n1[i + 1])*deltaT/(deltaX*deltaX) + T_n1[i];
+        //printf("RANK %d is working %d from %d is %f\n", myrank, i, sub_nodes, T_n1[i]);
+      }
+
+      #ifdef PRINT_TO_FILE
+        if(i % 10 == 0) {
+          MPI_Gather((void *)T_n1, sub_nodes, MPI_DOUBLE, (void *)t_n1, sub_nodes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+          if(!myrank) {
+            t_n = t_n1;
+            fprintf(fp, "plot '-' u 1:2 w linespoints lw 1\n");
+            for(j=0; j < nodes; j++) {
+              fprintf(fp, "%d %f\n",j, t_n[j]);
+            }
+            fprintf(fp, "end\n");
+            fprintf(fp, "pause 0.000001\n");
+          }
+        }
+      #endif
 
     }
     currentTime += deltaT;
   }
 
 
-if(!myrank) {
-      gettimeofday(&end, NULL);
-      printf("%ld\n", ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
-}
-
-/*
-  MPI_Gather((void *)T_n1, sub_nodes, MPI_DOUBLE,
-	       (void *)t_n1, sub_nodes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  if (!myrank)  {
-    printf("END:\n");
-    for (i=0; i<nodes; i++)
-      printf ("%g\n", t_n1[i]);
+  if(!myrank) {
+    gettimeofday(&end, NULL);
+    printf("TIME: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
   }
-*/
+
   printf("RANK: %d - EXIT\n", myrank);
   MPI_Finalize();
 
-#ifdef PRINT_TO_FILE
-  if(!myrank) {
-    fclose(fp);
-  }
-#endif
+  #ifdef PRINT_TO_FILE
+    if(!myrank) {
+      fclose(fp);
+    }
+  #endif
 
   exit(0);
 }
